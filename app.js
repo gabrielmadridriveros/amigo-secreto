a102d8d38b0e2b3c1e4775a1cc03412660b893b3
gabrielmadridriveros <gabriel.madridriveros@gmail.com>
2025-03-17 14:35:19 -0300

    Actualiza app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 
 let listaAmigoSecreto = [];
 
-
+function asignarTextoElemento(elemento, texto) {
+  let elementoHTML = document.querySelector(elemento);/// esta funcion asignar texto a un elemento
+  elementoHTML.innerHTML = texto;
+  return;
+}
 function agregarAmigo() {
     let nombreAmigoSecretoInput = document.getElementById('amigoInput');
     let nombreAmigoSecreto = nombreAmigoSecretoInput.value.trim();
@@ -9,10 +13,22 @@ function agregarAmigo() {
     if (nombreAmigoSecreto === "") {
         alert("Porfavor,inserte un nombre");
         return;
-    } else {
+    } 
+    if (listaAmigoSecreto.includes(nombreAmigoSecreto)) {
+      alert("Este nombre ya está en la lista.");
+      //limpiarCaja();
+      return;
+    }
+    else {
         listaAmigoSecreto.push(nombreAmigoSecreto);
         nombreAmigoSecretoInput.value = ""; 
-        actualizarListaAmigos();  
+        //limpiarCaja();
+        actualizarListaAmigos();
+
+
+    if (listaAmigoSecreto.length >= 3) {
+        document.querySelector(".button-draw").disabled = false;
+    }
     }
     
 
@@ -21,16 +37,25 @@ function agregarAmigo() {
 function actualizarListaAmigos() {
     let listaAmigos = document.getElementById('listaAmigos');
     listaAmigos.innerHTML = "";
+
+  /*  listaAmigoSecreto.forEach((amigo, index) => {
+      let item = document.createElement("li");
+      item.textContent = `${index + 1}. ${amigo}`;
+      lista.appendChild(item);
+  });*/
   
     for (let listaAmigoSecretos of listaAmigoSecreto) {
       let li = document.createElement('li');
       li.textContent = listaAmigoSecretos;
       listaAmigos.appendChild(li);
     }
-  }
+   }
+  function limpiarCaja() {
+    document.getElementById("listaAmigoSecreto").value ="";
+}
   function sortearAmigo() {
-    if (listaAmigoSecreto.length === 0) {
-      alert("No hay amigos en la lista para sortear.");
+    if (listaAmigoSecreto.length <= 2) {
+      alert("Se sequiere al menos 3 amigos para el sorteo.");
       return;
     }
 
@@ -38,6 +63,25 @@ function actualizarListaAmigos() {
     const amigoSorteado = listaAmigoSecreto[indiceAleatorio];
     const resultadoSorteo = document.getElementById('resultado');
     resultadoSorteo.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
-    
+    setTimeout(reiniciarJuego,5000);
   }
+
+  function reiniciarJuego() {
+    listaAmigoSecreto = []; // Vaciar la lista de amigos
+
+    // Limpiar la lista de amigos
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    // Limpiar el resultado del sorteo
+    let resultadoHTML = document.getElementById("resultado");
+    resultadoHTML.innerHTML = "";
+
+    // Deshabilitar el botón de sortear
+    document.querySelector(".button-draw").disabled = true;
+
+    // Limpiar la caja de texto
+    limpiarCaja();
+}
+ 
  
